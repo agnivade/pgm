c09131920cabdbf7b6ca8de1958106a82eccb96f
Agniva De Sarker <agnivade@yahoo.co.in>
2021-04-30 13:35:39 +0530

    Added search

--- pgm_index.go
+++ pgm_index.go
@@ -1,9 +1,16 @@
 package pgm
 
+import (
+	"errors"
+	"math"
+)
+
 // Index contains the PGM index.
 // It is just a hierarchy of segments.
 type Index struct {
-	levels [][]Segment
+	levels  [][]Segment
+	epsilon int
+	length  int // length of the array.
 }
 
 // Segment contains the slope and intercept of the line segment
@@ -14,15 +21,23 @@ type Segment struct {
 	intercept float64
 }
 
+type ApproxPos struct {
+	Pos int
+	Lo  int
+	Hi  int
+}
+
 type point struct {
 	x float64
 	y float64
 }
 
 // NewIndex returns a new static PGM index.
-func NewIndex(input []float64, epsilon float64) *Index {
+func NewIndex(input []float64, epsilon int) *Index {
 	index := &Index{
-		levels: make([][]Segment, 0),
+		levels:  make([][]Segment, 0),
+		epsilon: epsilon,
+		length:  len(input),
 	}
 	keys := make([]float64, len(input))
 	copy(keys, input)
@@ -50,7 +65,47 @@ func NewIndex(input []float64, epsilon float64) *Index {
 	return index
 }
 
-func buildPLAModel(keys []float64, epsilon float64) []Segment {
+func (ind *Index) Search(k float64) (ApproxPos, error) {
+	if len(ind.levels) < 1 || len(ind.levels[len(ind.levels)-1]) != 1 {
+		return ApproxPos{}, errors.New("invalid index")
+	}
+	root := ind.levels[len(ind.levels)-1][0]
+	pos := computePos(k, root)
+
+	for i := len(ind.levels) - 2; i >= 0; i-- {
+		lo := max(pos-ind.epsilon, 0)
+		hi := min(pos+ind.epsilon, len(ind.levels[i])-1)
+
+		var s, t Segment
+		// The rightmost segment s' in levels[i][lo, hi] such that s'.key ≤ k
+		for j, item := range ind.levels[i][lo : hi+1] {
+			if item.key <= k {
+				continue
+			}
+			s = ind.levels[i][max(lo+j-1, 0)]
+			t = item
+		}
+		if s.key == 0 {
+			s = ind.levels[i][hi]
+			t = s
+		}
+		pos = min(computePos(k, s), computePos(t.key, t))
+	}
+	// We pad the results by one to account
+	// for the inaccuracies created by rounding off.
+	// This needs to be fixed.
+	lo := max(pos-ind.epsilon-1, 0)
+	hi := min(pos+ind.epsilon+1, ind.length-1)
+	return ApproxPos{Lo: lo, Hi: hi, Pos: pos}, nil
+}
+
+func computePos(k float64, s Segment) int {
+	// TODO: The rounding off introduces inaccuracies.
+	// We need to use integers all the way.
+	return int(math.Round((k - s.intercept) / s.slope))
+}
+
+func buildPLAModel(keys []float64, epsilon int) []Segment {
 	model := []Segment{}
 	temp := []point{}
 	for i := 0; i < len(keys); i++ {
@@ -64,7 +119,7 @@ func buildPLAModel(keys []float64, epsilon float64) []Segment {
 		h := r.getHeight()
 		// fmt.Println("height", h)
 
-		if h > 2*epsilon {
+		if h > float64(2*epsilon) {
 			slope, intercept := r.slopeAndIntercept()
 			// Add to model
 			model = append(model, Segment{key: temp[0].y, slope: slope, intercept: intercept})
@@ -81,3 +136,16 @@ func buildPLAModel(keys []float64, epsilon float64) []Segment {
 	return model
 }
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
--- pgm_index_test.go
+++ pgm_index_test.go
@@ -9,56 +9,115 @@ import (
 )
 
 func TestPGMIndex(t *testing.T) {
-	input := []struct {
-		data     []float64
-		segments int // segments in the last level
-		epsilon  float64
+	testCases := []struct {
+		data      []float64
+		segments  int // segments in the last level
+		epsilon   int
+		searchMem []struct {
+			k       float64
+			present bool
+		}
+		searchPred []float64
 	}{
 		{
 			data:     []float64{2, 12, 15, 18, 23, 24, 29, 31, 34, 36, 38, 48, 55, 59, 60, 71, 73, 74, 76, 88, 95, 102, 115, 122, 123, 124, 158, 159, 161, 164, 165, 187, 189, 190},
 			segments: 5,
-			epsilon:  1.0,
+			epsilon:  1,
+			searchMem: []struct {
+				k       float64
+				present bool
+			}{
+				{k: 31, present: true},
+				{k: 32, present: false},
+				{k: 80, present: false},
+				{k: 95, present: true},
+				{k: 190, present: true},
+				{k: 200, present: false},
+			},
+			searchPred: []float64{1, 16, 15, 40, 48, 100, 200},
 		},
 		{
 			data:     []float64{21, 24, 46, 50, 52, 108, 109, 141, 147, 152, 178, 185, 275, 282, 310, 324, 332, 373, 380, 415, 433, 442, 452, 471, 476, 496},
 			segments: 3,
-			epsilon:  1.0,
+			epsilon:  1,
 		},
 		{
 			data:     []float64{1, 2, 13, 36, 37, 57, 69, 107, 140, 176, 215, 229, 246, 260, 288, 324, 337, 341, 381, 390, 409, 411, 416, 442, 444, 453, 476, 497},
 			segments: 3,
-			epsilon:  1.0,
+			epsilon:  1,
 		},
 		{
 			data:     []float64{11, 28, 119, 131, 167, 345, 348, 362, 369, 439},
 			segments: 2,
-			epsilon:  1.0,
+			epsilon:  1,
 		},
 	}
 
-	for _, item := range input {
-		sort.Float64s(item.data)
+	for _, tc := range testCases {
+		sort.Float64s(tc.data)
 
-		ind := NewIndex(item.data, item.epsilon)
+		ind := NewIndex(tc.data, tc.epsilon)
 		for _, level := range ind.levels {
 			fmt.Println(level)
 		}
 
-		if len(ind.levels[0]) != item.segments {
-			t.Errorf("incorrect number of segments. Got: %d, Want: %d", len(ind.levels[0]), item.segments)
+		if len(ind.levels[0]) != tc.segments {
+			t.Errorf("incorrect number of segments. Got: %d, Want: %d", len(ind.levels[0]), tc.segments)
 		}
-		verifyIndex(t, ind, item.data, item.epsilon)
+		verifyIndex(t, ind, tc.data, tc.epsilon)
+
+		if tc.searchMem != nil {
+			for _, mem := range tc.searchMem {
+				pos, err := ind.Search(mem.k)
+				if err != nil {
+					t.Errorf("error received: %v", err)
+				}
+
+				found := false
+				for _, d := range tc.data[pos.Lo : pos.Hi+1] {
+					if d == mem.k {
+						found = true
+						break
+					}
+				}
+
+				if found != mem.present {
+					t.Errorf("incorrect membership result for %f. Got %t, Want: %t", mem.k, found, mem.present)
+				}
+			}
+		}
+
+		if tc.searchPred != nil {
+			for _, pred := range tc.searchPred {
+				pos, err := ind.Search(pred)
+				if err != nil {
+					t.Errorf("error received: %v", err)
+				}
+				t.Log(pred, tc.data[pos.Lo], tc.data[pos.Hi])
+
+				if pos.Lo >= pos.Hi {
+					t.Errorf("lo %d is greater than hi %d", pos.Lo, pos.Hi)
+				}
+				// pos.Lo <= k <= pos.Hi
+				if tc.data[pos.Lo] > pred && pos.Lo != 0 {
+					t.Errorf("lo %f is greater than k %f", tc.data[pos.Lo], pred)
+				}
+				if tc.data[pos.Hi] < pred && pos.Hi != len(tc.data)-1 {
+					t.Errorf("hi %f is lesser than k %f", tc.data[pos.Hi], pred)
+				}
+			}
+		}
 	}
 }
 
-func verifyIndex(t *testing.T, ind *Index, input []float64, epsilon float64) {
-	var verifySegment = func(t *testing.T, set []float64, start, end int, s Segment) {
+func verifyIndex(t *testing.T, ind *Index, input []float64, epsilon int) {
+	verifySegment := func(t *testing.T, set []float64, start, end int, s Segment) {
 		t.Helper()
 		// Iterate all points in the segment and verify they are within e.
 		for i := start; i < end; i++ {
 			// (y -c)/m - key = err
 			err := ((set[i] - s.intercept) / s.slope) - float64(i)
-			if err > 2*epsilon {
+			if err > float64(2*epsilon) {
 				t.Errorf("error threshold exceeded, x: %d, y:%f", i, set[i])
 			}
 		}
